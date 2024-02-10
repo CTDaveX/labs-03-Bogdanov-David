245721cd211d4684e04044d127865c7b800df32e
CTDave <88465578+CTDave001@users.noreply.github.com>
2024-02-09 17:29:45 -0800

    Commit changes to lab01a assignment

--- src/lab01$arraylists/Test01ReverseWords.java
+++ src/lab01$arraylists/Test01ReverseWords.java
@@ -3,124 +3,59 @@ package lab01$arraylists;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * @author A. Ford and You
- * Create a runnable program with output as described in the .
- */
-
 public class Test01ReverseWords {
 
-    /**
-     * reads in an empty list and file location and populates list with
-     * the lines and words reversed.
-     * @param inputLocation for reading in the file information
-     * @param list to be populated
-     * @throws FileNotFoundException if file location not found
-     */
-    public static void read(String inputLocation, List<String> list) throws FileNotFoundException{
-        //TODO: check if input location exists
+    public static void read(String inputLocation, List<String> list) throws FileNotFoundException {
+        File file = new File(inputLocation);
+        Scanner scanner = new Scanner(file);
 
-        //TODO: Create a File Object
-
-        //TODO: Opens up a file input stream
-
-        //TODO: store words in reverse in a list from each line
-
-        //TODO: read in the data using input scanner
-
-            //TODO: read in next line and store as a string
-
-            //TODO: get each word on each line using a delimiter and store in index location
-
-            //TODO: store words in reverse
-
-            //TODO: use string builder to build out lines with space
-
-
-            //TODO: add each reversed line to list as a string
-
-            //TODO: clear the words on each line
-
-
-    }
-
-    /**
-     * checks whether the file exists
-     * @param location of file is checked
-     * @throws FileNotFoundException if file does not exist
-     */
-    public static void validateInputLocation(String location) throws FileNotFoundException{
-
-        File inputFile = new File(location);
-
-        if (!inputFile.exists()) {
-            throw new FileNotFoundException("File at " + location + " does not exist.");
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            List<String> wordsList = new ArrayList<>(List.of(line.split(" ")));
+            Collections.reverse(wordsList);
+            // Joining words back together
+            String reversedLine = String.join(" ", wordsList);
+            // Adding reversed lines to the beginning of the list to reverse their order
+            list.add(0, reversedLine);
         }
 
-        System.out.println("Processing \t" + location + "...");
-        
+        scanner.close();
     }
 
-    /**
-     * checks whether the output directory exists
-     * @param directory folder to place output
-     * @param location for output file
-     */
-    public static void validateOutputFolder(String directory, String location) {
+    public static void write(String outputLocation, List<String> list) throws FileNotFoundException {
+        // ensuring the directory exists is handled in a separate method for clearer separation of concerns
+        ensureDirectoryExists(outputLocation);
 
-        File outputFile = new File(location);
-
-        if (!outputFile.exists()) {
-
-            boolean hasCreatedDirectory = new File(directory).mkdir();
-            System.out.println("Has the directory been created? " + hasCreatedDirectory);
-            System.out.println();
-
-            if (hasCreatedDirectory) {
-                System.out.println("Processing \t" + directory + "...");
-            }else {
-                System.out.println("something went wrong.");
-            }
+        try (PrintStream printStream = new PrintStream(new File(outputLocation))) {
+            list.forEach(printStream::println);
         }
-        
     }
 
-    /**
-     * writes to an output file
-     * @param outputLocation to where output file exists
-     * @param list containing reversed words
-     * @throws FileNotFoundException when cannot write to output location
-     */
-    public static void write(String outputLocation, List<String> list) throws FileNotFoundException{
-        
-        //TODO: Open up a file output stream
-
-        // TODO: write out lines using  Print Stream -> write
-
-        //TODO: close output stream
+    private static void ensureDirectoryExists(String outputLocation) {
+        String directoryPath = outputLocation.substring(0, outputLocation.lastIndexOf(File.separator));
+        File directory = new File(directoryPath);
+        if (!directory.exists() && !directory.mkdirs()) {
+            System.err.println("Could not create the directory: " + directoryPath);
+        }
     }
 
+    public static void main(String[] args) {
+        String inputLocation = "." + File.separator + "data" + File.separator + "words.txt";
+        String outputLocation = "." + File.separator + "results" + File.separator + "reverse_words.txt";
 
-    public static void main(String[] args) throws FileNotFoundException {
-        String inputLocation    = "." + File.separator + "data"
-                                      + File.separator + "words.txt";
-
-        String outputFolder     = "." + File.separator + "results";
-
-        String outputLocation   = "." + File.separator + "results"
-                                      + File.separator + "reverse_words.txt";
-
-        // TODO: update program to use an Array List
-
-        // TODO: process items in file (REVERSALS, can be done either in main or in read)
-
-        //TODO: Validate Output Directory Exists
-
+        try {
+            List<String> list = new ArrayList<>();
+            read(inputLocation, list);
+            write(outputLocation, list);
+        } catch (FileNotFoundException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
     }
-
 }
