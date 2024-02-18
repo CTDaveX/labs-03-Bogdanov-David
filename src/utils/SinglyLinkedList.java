7564df2849c96ea06789a0414a86d8d7f129bc5e
CTDaveX <david.bogdanov@seattlecolleges.com>
2024-02-18 15:04:03 -0800

    Fixed the Singly Linked Lists Implementations

--- src/utils/SinglyLinkedList.java
+++ src/utils/SinglyLinkedList.java
@@ -2,75 +2,62 @@ package utils;
 
 public class SinglyLinkedList<T> {
 
-    private Node<T> front;
+    private Node<T> first;
     private int size;
 
     public SinglyLinkedList() {
-        front = null;
+        first = null;
         size = 0;
     }
 
-    public boolean add(T value) {
-        Node<T> newNode = new Node<>(value);
-        if (isEmpty()) {
-            front = newNode;
-        } else {
-            Node<T> endNode = node(size - 1);
-            endNode.next = newNode;
-        }
-        size++;
-        return true; // Indicate that the addition was successful
+    public boolean add(T item) {
+        append(item);
+        return true;
     }
 
-    public void add(int index, T value) {
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
         if (index == 0) {
-            front = new Node<>(value, front);
+            first = new Node<>(item, first);
         } else {
             Node<T> nodeBefore = node(index - 1);
-            Node<T> targetNode = nodeBefore.next;
-            nodeBefore.next = new Node<>(value, targetNode);
+            nodeBefore.next = new Node<>(item, nodeBefore.next);
         }
         size++;
     }
 
-    public void addAll(SinglyLinkedList<T> other) {
-        for (int i = 0; i < other.size; i++) {
-            add(other.get(i));
+    private void append(T item) {
+        if (isEmpty()) {
+            first = new Node<>(item);
+        } else {
+            Node<T> current = first;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(item);
         }
+        size++;
     }
 
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Invalid Index: " + index);
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-    }
-
-    public void clear() {
-        front = null;
-        size = 0;
     }
 
     public T get(int index) {
+        checkIndex(index);
         return node(index).data;
     }
 
-    public int indexOf(T value) {
-        int index = 0;
-        for (Node<T> node = front; node != null; node = node.next, index++) {
-            if (node.data.equals(value)) {
-                return index;
-            }
-        }
-        return -1;
-    }
-
     public boolean isEmpty() {
-        return front == null && size == 0;
+        return size == 0;
     }
 
     private Node<T> node(int index) {
-        checkIndex(index);
-        Node<T> current = front;
+        Node<T> current = first;
         for (int i = 0; i < index; i++) {
             current = current.next;
         }
@@ -79,17 +66,25 @@ public class SinglyLinkedList<T> {
 
     public T remove(int index) {
         checkIndex(index);
-        Node<T> toRemove;
+        T removedData;
         if (index == 0) {
-            toRemove = front;
-            front = front.next;
+            removedData = first.data;
+            first = first.next;
         } else {
             Node<T> prev = node(index - 1);
-            toRemove = prev.next;
-            prev.next = toRemove.next;
+            removedData = prev.next.data;
+            prev.next = prev.next.next;
         }
         size--;
-        return toRemove.data;
+        return removedData;
+    }
+
+    public T set(int index, T item) {
+        checkIndex(index);
+        Node<T> targetNode = node(index);
+        T oldData = targetNode.data;
+        targetNode.data = item;
+        return oldData;
     }
 
     public int size() {
@@ -97,35 +92,28 @@ public class SinglyLinkedList<T> {
     }
 
     public String toString() {
-        if (isEmpty()) {
-            return "[]";
-        } else {
-            StringBuilder result = new StringBuilder("[" + front.data);
-            for (Node<T> node = front.next; node != null; node = node.next) {
-                result.append(", ").append(node.data);
+        StringBuilder sb = new StringBuilder("[");
+        Node<T> current = first;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
             }
-            return result.append("]").toString();
+            current = current.next;
         }
-    }
-
-    public T set(int index, T element) {
-        checkIndex(index);
-        Node<T> target = node(index);
-        T oldData = target.data;
-        target.data = element;
-        return oldData;
+        sb.append("]");
+        return sb.toString();
     }
 
     private static class Node<T> {
         T data;
         Node<T> next;
 
-        // Constructor with only data argument
         public Node(T data) {
-            this(data, null);
+            this.data = data;
+            this.next = null;
         }
 
-        // Constructor with data and next node arguments
         public Node(T data, Node<T> next) {
             this.data = data;
             this.next = next;
