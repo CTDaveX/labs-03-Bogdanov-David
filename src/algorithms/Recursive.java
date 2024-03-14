1c1f5c0c71e2ef32ab881270e31a337f9b159e86
CTDaveX <david.bogdanov@seattlecolleges.com>
2024-03-13 20:57:11 -0700

    Implemented recursion methods

--- src/algorithms/Recursive.java
+++ src/algorithms/Recursive.java
@@ -1,87 +1,109 @@
 package algorithms;
 
-/**
- * FIXME: Complete the following famous algorithms with their method body implementations
- *        It is suggested that you type them out by memory-ish to see if you understand the
- *        algorithm.
- */
-
 public class Recursive {
 
     public static int binarySearch(int[] data, int target) {
-        return 0;
+        return binarySearch(data, target, 0, data.length - 1);
     }
 
     private static int binarySearch(int[] data, int target, int min, int max) {
-        return 0;
+        if (min > max) {
+            return -1;  // Target not found
+        }
+        int mid = min + (max - min) / 2;
+        if (data[mid] == target) {
+            return mid;
+        } else if (data[mid] < target) {
+            return binarySearch(data, target, mid + 1, max);
+        } else {
+            return binarySearch(data, target, min, mid - 1);
+        }
     }
 
-    //TODO: Complete the following helper method for the corresponding
-    //      sorting algorithm
-    private static int[] getFirstHalf(int[] data) {
-        //TODO : update to get first half of array
-        return null;
-    }
-
-    //TODO: Complete the following helper method for the corresponding
-    //      sorting algorithm
-    private static int[] getSecondHalf(int[] data) {
-        //TODO : update to get second half of array
-        return null;
-    }
-
-
-    //TODO: Complete the following helper method for the corresponding
-    //      sorting algorithm
-    private static void merge(int[] data, int[] left, int[] right) {
-        //TODO: complete body
-    }
-
-    //TODO: Complete the following sorting algorithm
     public static void mergeSort(int[] data) {
-        //TODO: RECURSIVE CASE
-        //TODO: complete body
-
+        if (data.length > 1) {
+            int[] left = getFirstHalf(data);
+            int[] right = getSecondHalf(data);
+            mergeSort(left);
+            mergeSort(right);
+            merge(data, left, right);
+        }
     }
 
-    //TODO: Complete the following helper method for the corresponding
-    //      sorting algorithm
-    private static int partition(int[] data, int low, int high) {
-        //TODO: update with partition algorithm
-
-        return 0;
+    private static int[] getFirstHalf(int[] data) {
+        int[] left = new int[data.length / 2];
+        System.arraycopy(data, 0, left, 0, left.length);
+        return left;
     }
 
+    private static int[] getSecondHalf(int[] data) {
+        int[] right = new int[data.length - data.length / 2];
+        System.arraycopy(data, data.length / 2, right, 0, right.length);
+        return right;
+    }
 
-    //TODO: Complete the following sorting algorithm
+    private static void merge(int[] data, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                data[k++] = left[i++];
+            } else {
+                data[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            data[k++] = left[i++];
+        }
+        while (j < right.length) {
+            data[k++] = right[j++];
+        }
+    }
+
     public static void quickSort(int[] data) {
-        //TODO : update to call helper method
+        quickSort(data, 0, data.length - 1);
     }
 
-
-    //TODO: Complete the following helper method for the corresponding
-    //          sorting algorithm
     private static void quickSort(int[] data, int min, int max) {
-        //TODO: update to partition list
+        if (min < max) {
+            int pivotIndex = partition(data, min, max);
+            quickSort(data, min, pivotIndex - 1);
+            quickSort(data, pivotIndex + 1, max);
+        }
     }
 
+    private static int partition(int[] data, int low, int high) {
+        int pivot = data[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (data[j] < pivot) {
+                i++;
+                swap(data, i, j);
+            }
+        }
+        swap(data, i + 1, high);
+        return i + 1;
+    }
 
-    //TODO: Complete the following sorting algorithm
     public static void selectionSort(int[] data) {
-        //TODO: complete body
+        selectionSort(data, 0);
     }
 
-
-    //TODO: Update/Complete the following selectionSort algorithm
-    //      using for loops and swap method (see below)
     private static void selectionSort(int[] data, int start) {
-        //TODO: complete body
+        if (start < data.length - 1) {
+            int minIndex = start;
+            for (int i = start + 1; i < data.length; i++) {
+                if (data[i] < data[minIndex]) {
+                    minIndex = i;
+                }
+            }
+            swap(data, start, minIndex);
+            selectionSort(data, start + 1);
+        }
     }
 
-    //TODO: Complete and use the following swap method for
-    //      sorting algorithm that require swapping of data
     public static void swap(int[] data, int a, int b) {
-        //TODO: complete body
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
     }
-
 }
